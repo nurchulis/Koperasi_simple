f76c0c2a373ea1472a98bb12963d3c95ea4141bc
nurchulis <nura3609@gmail.com>
2018-04-17 08:35:41 -0700

    tambah sql

--- Koperasi.sql
+++ Koperasi.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.7
+-- version 4.7.9
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Apr 15, 2018 at 10:08 AM
--- Server version: 10.1.30-MariaDB
--- PHP Version: 5.6.33
+-- Host: 127.0.0.1
+-- Generation Time: Apr 17, 2018 at 05:34 PM
+-- Server version: 10.1.31-MariaDB
+-- PHP Version: 5.6.34
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,36 +19,57 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `Koperasi`
+-- Database: `koperasi`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Anggota`
+-- Table structure for table `anggota`
 --
 
-CREATE TABLE `Anggota` (
+CREATE TABLE `anggota` (
   `Noinduk` int(5) NOT NULL,
   `Nama` varchar(25) NOT NULL,
   `JK` varchar(2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `Anggota`
+-- Dumping data for table `anggota`
 --
 
-INSERT INTO `Anggota` (`Noinduk`, `Nama`, `JK`) VALUES
+INSERT INTO `anggota` (`Noinduk`, `Nama`, `JK`) VALUES
 (1, 'Nurchulis', 'L'),
-(2, 'Nuranita', 'P');
+(2, 'Nuraini', 'P');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Pinjaman`
+-- Table structure for table `angsuran`
 --
 
-CREATE TABLE `Pinjaman` (
+CREATE TABLE `angsuran` (
+  `id_angsuran` int(6) NOT NULL,
+  `Noinduk` varchar(6) NOT NULL,
+  `tgl` date NOT NULL,
+  `jumlahangsuran` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `angsuran`
+--
+
+INSERT INTO `angsuran` (`id_angsuran`, `Noinduk`, `tgl`, `jumlahangsuran`) VALUES
+(1, '1', '2018-04-20', '100000'),
+(2, '1', '2018-04-21', '200000');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pinjaman`
+--
+
+CREATE TABLE `pinjaman` (
   `Nopinjam` int(5) NOT NULL,
   `Tgl` date NOT NULL,
   `Noinduk` int(5) NOT NULL,
@@ -56,46 +77,91 @@ CREATE TABLE `Pinjaman` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `Pinjaman`
+-- Dumping data for table `pinjaman`
 --
 
-INSERT INTO `Pinjaman` (`Nopinjam`, `Tgl`, `Noinduk`, `Jumlahpinjam`) VALUES
+INSERT INTO `pinjaman` (`Nopinjam`, `Tgl`, `Noinduk`, `Jumlahpinjam`) VALUES
 (1, '2018-04-11', 1, '500000'),
 (2, '2018-04-11', 1, '200000'),
 (3, '2018-04-11', 2, '600000'),
 (4, '2018-04-11', 2, '600000');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `simpanan`
+--
+
+CREATE TABLE `simpanan` (
+  `id_simpanan` int(5) NOT NULL,
+  `Noinduk` int(5) NOT NULL,
+  `tgl` date NOT NULL,
+  `jumlahsimpanan` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `simpanan`
+--
+
+INSERT INTO `simpanan` (`id_simpanan`, `Noinduk`, `tgl`, `jumlahsimpanan`) VALUES
+(1, 2, '2018-04-09', '3000000'),
+(2, 2, '2018-04-18', '1000000');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `Anggota`
+-- Indexes for table `anggota`
 --
-ALTER TABLE `Anggota`
+ALTER TABLE `anggota`
   ADD PRIMARY KEY (`Noinduk`);
 
 --
--- Indexes for table `Pinjaman`
+-- Indexes for table `angsuran`
 --
-ALTER TABLE `Pinjaman`
+ALTER TABLE `angsuran`
+  ADD PRIMARY KEY (`id_angsuran`);
+
+--
+-- Indexes for table `pinjaman`
+--
+ALTER TABLE `pinjaman`
   ADD PRIMARY KEY (`Nopinjam`);
+
+--
+-- Indexes for table `simpanan`
+--
+ALTER TABLE `simpanan`
+  ADD PRIMARY KEY (`id_simpanan`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `Anggota`
+-- AUTO_INCREMENT for table `anggota`
 --
-ALTER TABLE `Anggota`
+ALTER TABLE `anggota`
   MODIFY `Noinduk` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `Pinjaman`
+-- AUTO_INCREMENT for table `angsuran`
 --
-ALTER TABLE `Pinjaman`
+ALTER TABLE `angsuran`
+  MODIFY `id_angsuran` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `pinjaman`
+--
+ALTER TABLE `pinjaman`
   MODIFY `Nopinjam` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `simpanan`
+--
+ALTER TABLE `simpanan`
+  MODIFY `id_simpanan` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
