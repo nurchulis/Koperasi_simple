f4d11557f161324fbdcee0a0003f33cb436ee78b
nurchulis <nura3609@gmail.com>
2018-04-18 11:39:30 +0700

    fix modul

--- application/models/M_data.php
+++ application/models/M_data.php
@@ -2,16 +2,16 @@
 
 class M_data extends CI_Model{
 	function tampil_data(){
-	return $this->db->query("SELECT Anggota.Noinduk, Anggota.JK, Anggota.Nama, Pinjaman.Nopinjam, Pinjaman.Tgl, Pinjaman.Noinduk, Pinjaman.Jumlahpinjam, SUM(Pinjaman.Jumlahpinjam) ini from Pinjaman,Anggota where Anggota.Noinduk = Pinjaman.Noinduk GROUP BY Pinjaman.Noinduk,Anggota.Noinduk");
+	return $this->db->query("SELECT anggota.Noinduk, anggota.JK, anggota.Nama, pinjaman.Nopinjam, pinjaman.Tgl, pinjaman.Noinduk, pinjaman.Jumlahpinjam, SUM(pinjaman.Jumlahpinjam) ini from pinjaman,anggota where anggota.Noinduk = pinjaman.Noinduk GROUP BY pinjaman.Noinduk,anggota.Noinduk");
 	}
 	function tampil_data2(){
-	return $this->db->query("SELECT Anggota.Noinduk, Anggota.JK, Anggota.Nama, angsuran.id_angsuran, angsuran.Tgl, angsuran.Noinduk, angsuran.jumlahangsuran, SUM(angsuran.jumlahangsuran) ini from angsuran,Anggota where Anggota.Noinduk = angsuran.Noinduk GROUP BY angsuran.Noinduk,Anggota.Noinduk");
+	return $this->db->query("SELECT anggota.Noinduk, anggota.JK, anggota.Nama, angsuran.id_angsuran, angsuran.Tgl, angsuran.Noinduk, angsuran.jumlahangsuran, SUM(angsuran.jumlahangsuran) ini from angsuran,anggota where anggota.Noinduk = angsuran.Noinduk GROUP BY angsuran.Noinduk,anggota.Noinduk");
 	}
 	function tampil_data3(){
 		return $this->db->get('anggota');
 	}
 	function tampil_data4(){
-	return $this->db->query("SELECT Anggota.Noinduk, Anggota.JK, Anggota.Nama, simpanan.id_simpanan, simpanan.tgl, simpanan.Noinduk, simpanan.jumlahsimpanan, SUM(simpanan.jumlahsimpanan) ini from simpanan,Anggota where Anggota.Noinduk = simpanan.Noinduk GROUP BY simpanan.Noinduk,Anggota.Noinduk");
+	return $this->db->query("SELECT anggota.Noinduk, anggota.JK, anggota.Nama, simpanan.id_simpanan, simpanan.tgl, simpanan.Noinduk, simpanan.jumlahsimpanan, SUM(simpanan.jumlahsimpanan) ini from simpanan,anggota where anggota.Noinduk = simpanan.Noinduk GROUP BY simpanan.Noinduk,anggota.Noinduk");
 	}
 }
 ?>
